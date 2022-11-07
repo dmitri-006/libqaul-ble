51053c45ba4c38cd7395bc7bdb74ab6141cbc564
dmitri-006 <dragon.steal75@gmail.com>
2022-11-07 18:31:37 +0000

    updated - main->libqaul->blemodule(with param)

--- rust/libqaul/src/lib.rs
+++ rust/libqaul/src/lib.rs
@@ -1,4 +1,4 @@
-use jni::objects::JClass;
+use jni::objects::{JClass, JValue, JObject};
 use jni::JNIEnv;
 
 #[no_mangle]
@@ -7,11 +7,20 @@ pub extern "system" fn Java_net_qaul_libqaul_LibQaul_hello(
     env: JNIEnv,
     _calss: JClass,
 ) {
+    let width = 100;
+    let height = 100;
+    let b_array = env.new_byte_array(width*height*3);
+    // let array_obj:JObject = JObject::from(b_array);
+
     let class = env
         // .find_class("net/qaul/libqaul/LibQaul") // main->libqaul->main
         .find_class("net/qaul/ble/core/BleWrapperClass") // main->libqaul->blemodule
         .expect("Failed to load the target class");
-    let result = env.call_static_method(class, "receiveRequest", "()V", &[]);
+    let result = env.call_static_method(class, "receiveRequest", "(II)V", &[
+        JValue::from(width),
+        JValue::from(height),
+        // JValue::from(array_obj),
+    ]);
 
     result.map_err(|e| e.to_string()).unwrap();
 }
