539de92a85bc71130e52b6d6edcbddbc6bc9240b
dmitri-006 <dragon.steal75@gmail.com>
2022-11-09 18:24:40 +0000

    added functions - syssend, receiveRequest

--- rust/libqaul/src/lib.rs
+++ rust/libqaul/src/lib.rs
@@ -17,22 +17,6 @@ pub extern "system" fn Java_net_qaul_libqaul_LibqaulKt_hello(
     
     // return the raw pointer
     output.into_inner()
-
-    // let width = 200;
-    // let height = 50;
-    // let b_array = env.new_byte_array(width*height*3);
-    // // let array_obj:JObject = JObject::from(b_array);
-
-    // let class = env
-    //     .find_class("net/qaul/ble/core/BleWrapperClass") // main->libqaul->blemodule
-    //     .expect("Failed to load the target class");
-    // let result = env.call_static_method(class, "receiveRequest", "(II)V", &[
-    //     JValue::from(width),
-    //     JValue::from(height),
-    //     // JValue::from(array_obj),
-    // ]);
-
-    // result.map_err(|e| e.to_string()).unwrap();
 }
 
 /// # BLE Module Functions
